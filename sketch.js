5a5024b111ecaba49ffeb364b6500ef8f10c989f
nicoleal88 <nicoleal88@gmail.com>
2020-06-24 12:16:53 -0300

    Update

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 // ToDo
-// - Add References
-// - Add Roads
-// - Github.io
-// - Add classification of SDs (Araya, 433, AERALet, etc.) 
+// - Add more data (FrontEnd, BBox, Batteries, etc.)
+// - Add button to export
+// - Add trip button
+// - Add selection on click
+// - Add table with selected SDs properties
+// - Add SD png image
+// - Add title of the things being plotted
+// - Add UMDs from UC
+// - Fix UMDs on top-left corner
 
 // Done
+// - Add References 
+// - Add Roads
 // - Add UMDs
 // - Make objects?
-// - Add more data (FrontEnd, BBox, Batteries, etc.)
+// - Github.io
+// - Add classification of SDs (Araya, 433, AERALet, etc.) 
+// - Divide GUI into categories
+
 
 //Data
 let table;
@@ -15,70 +25,66 @@ let tableObject;
 let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS3WkkXWxUp3TXEBsqGeeAtuMNKwVu3ZPASyzY8C43B5fWEyKqp2Xs0sEcM3_VXy_eoJNI_a8Mo8aiN/pub?gid=182439664&single=true&output=csv"
 let data = [];
 let tanks = [];
+let roads = [];
+let tracks = [];
+let roadsFile;
+let tracksFile;
+// let sd_img;
 
 //Map settings
 let AMIGA_Map;
 let canvas;
-const mappa = new Mappa('Leaflet');
+// Mapbox API key
+var key = 'pk.eyJ1Ijoibmljb2xlYWw4OCIsImEiOiJjazA3NWRmaHYzdjM5M2xwMHhoeGEwcnNhIn0.U9_rp4dKVkuTWEHODTHdgg';
+var mappa = new Mappa('MapboxGL', key);
+// const mappa = new Mappa('Leaflet');
 
 //Colors
-let colors = {
-  ok : "limegreen",
-  warning : "yellow",
-  dead : "red",
-  noData : "silver"
-}
+let colors;
+let roadsColor;
 
-//GUI settings
-// var strokeWidth = 1;
-// var strokeColor = '#FFFFFF';
-var item = [
-  'cap_hs',
-  'ip',
-  'radio_uptime',
-  'front_end',
-  'amiga_box',
-  'bbox',
-  'terminado'
-];
-
-var showLabel = true;
-var showName = false;
-var showLSID = true;
-var showUC = false;
-var showUMDs = false;
-
-var show433 = true;
-var showTwins_KT = false;
-var showCampoIbarra = true;
-var showCampoAraya = true;
-
-var mult = 25;
-
-// gui
-var visible = true;
-var gui;
+// New gui
+let newGUI;
+let items;
+let showInfo;
+let showHexagons;
+let showSDs;
+let mult;
 
 // Lets put all our map options in a single object
 
-const options = {
+// Leaflet options
+// const options = {
+//   lat: -35.11631067505913,
+//   lng: -69.53360985611256,
+//   // lat: -35.110417,
+//   // lng: -69.537750,
+//   zoom: 14,
+//   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   scl : 0.00001
+// }
+
+// Mapbox options
+var options = {
   lat: -35.11631067505913,
   lng: -69.53360985611256,
-  // lat: -35.110417,
-  // lng: -69.537750,
-  zoom: 14,
-  style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  zoom: 13.2,
+  // style: "mapbox://styles/mapbox/satellite-streets-v9",
+  style: "mapbox://styles/mapbox/dark-v9",
+  pitch: 0,
+  bearing: 0,
+  minZoom: 1,
+  maxZoom: 20,
+  renderWorldCopies: false,
+  scl : 0.00002
 }
 
-let UC = ['93',
-          '1574',
-          '1570',
-          '688',
-          '1764',
-          '1773',
-          '93'
-          ];
+// Hexagons
+let UC = ['93', '1574', '1570', '688', '1764', '1773', '93'];
+let MARTA = ['1764', '688', '1760', '1767', '669', '1765', '1764'];
+let h433_1 = ['30', '12', '97', '47', '99', '11', '30'];
+let h433_2 = ['27', '29', '28', '54', '50', '42', '27'];
 
 // const malargue = {
 //   // Malargue coords: -35.46667,-69.58333
@@ -88,39 +94,96 @@ let UC = ['93',
 
 function preload() {
   table = loadTable(url, "csv", "header");
+  roadsFile = loadStrings("files/Rutas.dat");
+  tracksFile = loadStrings("files/Tracks-AERA.dat");
+  // sd_img = loadImage("files/SD.png");
 }
 
 function setup() {
-  textAlign(CENTER, CENTER);
   // canvas = createCanvas(640, 640);
-  canvas = createCanvas(windowHeight, windowHeight);
-
+  let minSize = min(windowHeight, windowWidth);
+  canvas = createCanvas(minSize, minSize);
+  canvas.parent('sketch-div');
   frameRate(10);
-
-  console.log(table);
+  
+  colors = {
+    ok: "#2ECC40", // green
+    warning: "#FFDC00", // yellow
+    dead: "#FF4136", // red
+    noData: "silver",
+    roads : color(255, 204, 0, 50),
+    selected : "#FF851B" // Orange
+  }
+  
+  // console.log(table);
+  // console.log(roadsFile);
+  roads = loadRoads(roadsFile);
+  tracks = loadRoads(tracksFile);
   // tableObject = table.getObject();
   // Create a tile map with the options declared
   AMIGA_Map = mappa.tileMap(options);
   AMIGA_Map.overlay(canvas);
 
-  // Create Layout GUI
-  gui = createGui();
-  // gui.setPosition(650,100);
-  gui.setPosition(windowHeight + 5, 0);
+  // New gui config
+  newGUI = new dat.GUI();
 
-  // gui.setPosition(10,10);
-  sliderRange(1, 50, 1);
-  gui.addGlobals('item',
-    'showLSID',
-    'showName',
-    'showLabel',
-    'showUC',
-    'showUMDs',
-    'show433',
-    'showTwins_KT',
-    'showCampoIbarra',
-    'showCampoAraya',
-    'mult');
+  propiedades = {
+    item: "cap_disipador",
+    mult: 25,
+    screenshot: takeScreenshot
+  };
+
+  newGUI.add(propiedades, 'item', ['cap_disipador',
+                                    'ip',
+                                    'radio_uptime',
+                                    'front_end',
+                                    'amiga_box',
+                                    'terminado',
+                                    'observaciones'
+                                  ])
+  newGUI.add(propiedades, 'mult', 1, 50);
+
+  newGUI.add(propiedades, 'screenshot');
+
+  let infoFolder = newGUI.addFolder("Show info");
+  showInfo = {
+    showLabel: true,
+    showName: false,
+    showLSID: true,
+    showUMDs: false,
+    showRoads: false
+  };
+  infoFolder.add(showInfo, 'showName');
+  infoFolder.add(showInfo, 'showLSID');
+  infoFolder.add(showInfo, 'showLabel');
+  infoFolder.add(showInfo, 'showUMDs');
+  infoFolder.add(showInfo, 'showRoads');
+
+  let hexagonsFolder = newGUI.addFolder("Show hexagons");
+  showHexagons = {
+    showUC: false,
+    showMARTA: false,
+    show433_1: false,
+    show433_2: false
+  }
+  hexagonsFolder.add(showHexagons, 'showUC');
+  hexagonsFolder.add(showHexagons, 'showMARTA');
+  hexagonsFolder.add(showHexagons, 'show433_1');
+  hexagonsFolder.add(showHexagons, 'show433_2');
+
+  let sdFolder = newGUI.addFolder("Show SDs");
+  showSDs = {
+    show433: true,
+    showTwins_KT: false,
+    showCampoIbarra: true,
+    showCampoAraya: true
+  }
+  sdFolder.add(showSDs, 'show433');
+  sdFolder.add(showSDs, 'showTwins_KT');
+  sdFolder.add(showSDs, 'showCampoIbarra');
+  sdFolder.add(showSDs, 'showCampoAraya');
+
+  infoFolder.open();
 
   // Data loading
   for (let row of table.rows) {
@@ -129,12 +192,13 @@ function setup() {
     let radio_mikrotik = row.get('Radio_Mikrotik');
     let ip = row.get('IP');
     let amiga_box = row.get('AMIGA_Box');
-    let cap_hs = row.get('Cap_HS');
+    let cap_disipador = row.get('Cap_HS');
     let terminado = row.get('Terminado');
     let radio_uptime = row.get('Radio_Uptime');
     let front_end = row.get('Front_End');
     let bbox = row.get('BBox');
     let tipo = row.get('Tipo');
+    let observaciones = row.get('Observaciones');
 
     //UMDs data
     let id1, id2, id3;
@@ -157,10 +221,10 @@ function setup() {
     ekit1 = row.get('eKit_M101');
     ekit2 = row.get('eKit_M102');
     ekit3 = row.get('eKit_M103');
-    
+
     let tx = row.get('TX');
     let dist = row.get('Distrib.');
-    
+
     // Conversion from UTM to LatLng
     let utmz = 19;
     let easting = Number(row.get('LAT'));
@@ -190,17 +254,18 @@ function setup() {
       pa3,
       radio_mikrotik,
       ip,
-      cap_hs,
+      cap_disipador,
       radio_uptime,
       front_end,
-      ekit1, 
-      ekit2, 
+      ekit1,
+      ekit2,
       ekit3,
       amiga_box,
       tx,
       dist,
       bbox,
       tipo,
+      observaciones,
       terminado
     }
 
@@ -209,70 +274,125 @@ function setup() {
     let tank = new Tank(datos);
     tanks.push(tank);
   }
-  console.log(data);
+  // console.log(data);
   console.log(tanks);
   // AMIGA_Map.onChange(drawMap);
-
 }
 
 function draw() {
   clear();
-
   // Zoom settings
   const zoom = AMIGA_Map.zoom();
   const scl1 = pow(2, zoom);
-  const sclm = 0.00001; // Escala para unidades en metros
+  // const sclm =  // Escala para unidades en metros Leaflet
+  const sclm = options.scl // Escala para unidades en metros Mapbox
   // const scl2 = 0.0002;
   // const offset = radius * 1.5;
   // let escala = constrain(scl * scl2, 2, 6);
   let escalaReal = scl1 * sclm;
 
-  for (let i = 0; i < tanks.length; i++){
+  // Disable SDs plotting:
+  for (let i = 0; i < tanks.length; i++) {
     tanks[i].update(); // Updates the position on the map
 
-    if (show433 == false && tanks[i].tipo == '433m'){
-      tanks[i].update(); 
-    }
-    else if (showTwins_KT == false && tanks[i].tipo == 'Twins_KT'){
-      tanks[i].update(); 
-    }
-    else if (showCampoIbarra == false && tanks[i].tipo == 'Campo_Ibarra'){
-      tanks[i].update(); 
-    }
-    else if (showCampoAraya == false && tanks[i].tipo == 'Campo_Araya'){
-      tanks[i].update(); 
-    }
-    else{
-      tanks[i].showSD(escalaReal, item, showLabel, showName, showLSID); // Plots with a certain scale
-    }
-
-    
-    if(showUMDs){
-      if (tanks[i].terminado){    // If the position is finished, and the checkbox is enabled:
-        tanks[i].showUMD(escalaReal); // Show the UMDs
-      }
+    if (showSDs.show433 == false && tanks[i].tipo == '433m') {
+      tanks[i].plot = false;
+    } else if (showSDs.showTwins_KT == false && tanks[i].tipo == 'Twins_KT') {
+      tanks[i].plot = false;
+    } else if (showSDs.showCampoIbarra == false && tanks[i].tipo == 'Campo_Ibarra') {
+      tanks[i].plot = false;
+    } else if (showSDs.showCampoAraya == false && tanks[i].tipo == 'Campo_Araya') {
+      tanks[i].plot = false;
+    } else {
+      tanks[i].plot = true;
     }
   }
 
-  if (showUC) {
-    drawShape(UC);
+  // Plot SDs
+  for (let i = 0; i < tanks.length; i++) {
+    if (tanks[i].plot == true){
+      tanks[i].showSD(escalaReal, propiedades.item, showInfo.showLabel, showInfo.showName, showInfo.showLSID); // Plots with a certain scale
+    }
   }
+
+  // Plot UMDs
+  for (let i = 0; i < tanks.length; i++) {
+    if (tanks[i].plot == true){
+      if (showInfo.showUMDs) {
+        if (tanks[i].terminado) { // If the position is finished, and the checkbox is enabled:
+          tanks[i].showUMD(escalaReal); // Show the UMDs
+          }
+        }
+    }
+  }
+
+  // Plot popups
+  for (let i = 0; i < tanks.length; i++) {
+    if (tanks[i].plot == true){
+    tanks[i].update(); // Updates the position on the map
+    tanks[i].showPopup();
+    }
+  }
+
+  // Plot hexagons
+  if (showHexagons.showUC) {
+    drawShape(UC, "black");
+  }
+  if (showHexagons.showMARTA) {
+    drawShape(MARTA, "orange");
+  }
+  if (showHexagons.show433_1) {
+    drawShape(h433_1, "blue");
+  }
+  if (showHexagons.show433_2) {
+    drawShape(h433_2, "cyan");
+  }
+
+  // Plot roads and tracks
+  // if (showInfo.showRoads) {
+  //   drawRoads(roads, "white");
+  // }
+  if (showInfo.showRoads) {
+    drawRoads(tracks, colors.roads);
+  }
+
+  // Plot References
+  showReferences();
+  showTitle(propiedades.item);
+
   // noLoop();
 }
 
-function keyPressed() {
-  switch (key) {
-    // type [p] to hide / show the GUI
-    case 'p':
-      visible = !visible;
-      if (visible) gui.show();
-      else gui.hide();
-      break;
+// function keyPressed() {
+//   switch (key) {
+//     // type [p] to hide / show the GUI
+//     case 'p':
+//       visible = !visible;
+//       if (visible) gui.show();
+//       else gui.hide();
+//       break;
+//   }
+// }
+
+function mouseClicked(){
+  for (let i = 0; i < tanks.length; i++) {
+    if (tanks[i].plot == true){
+    tanks[i].selectSD(); // Updates the position on the map
+    // console.log(tanks[i].selected);
+    }
   }
 }
 
-function drawShape(lista) {
-  let puntos = []
+function takeScreenshot(){
+  push();
+
+  save('myCanvas.png');
+  pop();
+  return false;
+}
+
+function drawShape(lista, col) {
+  let puntos = [];
   for (var i of lista) {
     for (var j = 0; j < data.length; j++) {
       if (data[j].lsid == i) {
@@ -280,21 +400,185 @@ function drawShape(lista) {
       }
     }
   }
-  strokeWeight(1);
+  push();
+  strokeWeight(2);
   noFill();
-  stroke(127);
+  stroke(col);
   beginShape();
   for (var elt of puntos) {
     const point = AMIGA_Map.latLngToPixel(elt.lat, elt.lng);
     vertex(point.x, point.y);
+  }
+  endShape();
+  pop();
+}
+
+function showReferences() {
+  let width = 120;
+  let height = 160;
+  textAlign(LEFT, CENTER);
+  push();
+  stroke(127);
+  strokeWeight(1);
+  fill(51, 200);
+  rect(canvas.width - width, 0, width, height, 5);
+  pop();
+  push();
+  fill(200);
+  noStroke();
+  textSize(16);
+  textAlign(LEFT);
+  text("References:", canvas.width - width + 5, 15);
+  
+  textSize(14);
+  fill(colors.ok);
+  circle(canvas.width - width + 8, 45, 10);
+  text("  : OK", canvas.width - width + 5, 45);
+  fill(colors.warning);
+  circle(canvas.width - width + 8, 75, 10);
+  text("  : Needs fix", canvas.width - width + 5, 75);
+  fill(colors.dead);
+  circle(canvas.width - width + 8, 105, 10);
+  text("  : Critical", canvas.width - width + 5, 105);
+  fill(colors.noData);
+  circle(canvas.width - width + 8, 135, 10);
+  text("  : No Data", canvas.width - width + 5, 135);
+  pop();
+  // const point = AMIGA_Map.latLngToPixel(elt.lat, elt.lng);
+}
+
+function showTitle(text_) {
+  let t = text_.replace("_", " ");
+  t = toTitleCase(t);
+  let width = t.length * 10 + 5;
+  let height = 30;
+  push();
+  stroke(127);
+  strokeWeight(1);
+  fill(51, 200);
+  rectMode(CENTER);
+  rect(canvas.width/2 , height * 0.5 + 5 , width, height, 5);
+  pop();
+  push();
+  fill(200);
+  noStroke();
+  textSize(16);
+  textAlign(CENTER, CENTER);
+  text(t, canvas.width /2, height * 0.5 + 5);
+  pop();
+  // const point = AMIGA_Map.latLngToPixel(elt.lat, elt.lng);
+}
+
+function loadRoads(file) {
+  let allroads = [];
+  let road = []
+  for (i = 0; i < file.length; i++) {
+    let line = file[i].split(/\s+/);
+    if (line.length == 2) {
+      let pointUTM = {
+        latUTM: line[0],
+        lngUTM: line[1]
+      }
+      let utmz = 19;
+      let easting = Number(pointUTM.latUTM);
+      let northing = Number(pointUTM.lngUTM) - 10000000;
+      var utm = new UTMConv.UTMCoords(utmz, easting, northing);
+      var degd = utm.to_deg();
+      var pos = {
+      lat: degd.latd,
+      lng: degd.lngd
+    }
+      road.push(pos);
+    } else {
+      allroads.push(road);
+      road = []
+    }
+  }
+  allroads.push(road);
+  // console.log(allroads);
+  return allroads;
+}
+
+function drawRoads(list, col) {
+  for (i = 0; i < list.length; i++) {
+    push();
+    strokeWeight(2);
+    noFill();
+    stroke(col);
+    beginShape();
+    for (var elt of list[i]) {
+      const point = AMIGA_Map.latLngToPixel(elt.lat, elt.lng);
+      vertex(point.x, point.y);
+    }
     endShape();
+    pop();
   }
 }
 
-function addText() {
-  const point = AMIGA_Map.latLngToPixel(elt.lat, elt.lng);
+function toTitleCase(str) {
+  return str.replace(
+      /\w\S*/g,
+      function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+  );
 }
 
 // function windowResized() {
 //   resizeCanvas(windowHeight, windowHeight);
 // }
+
+//GUI settings
+// var strokeWidth = 1;
+// var strokeColor = '#FFFFFF';
+
+// let item = [
+//   'cap_hs',
+//   'ip',
+//   'radio_uptime',
+//   'front_end',
+//   'amiga_box',
+//   'bbox',
+//   'terminado'
+// ];
+
+// var showLabel = true;
+// var showName = false;
+// var showLSID = true;
+// var showUMDs = false;
+
+// var showUC = false;
+// var showMARTA = false;
+// var show433_1 = false;
+// var show433_2 = false;
+
+// var show433 = true;
+// var showTwins_KT = false;
+// var showCampoIbarra = true;
+// var showCampoAraya = true;
+
+// gui
+// var visible = true;
+// var gui;
+
+// Create Layout GUI
+// gui = createGui();
+// gui.setPosition(650,100);
+// gui.setPosition(windowHeight + 5, 0);
+
+// gui.setPosition(10,10);
+// sliderRange(1, 50, 1);
+// gui.addGlobals('item',
+//   'showLSID',
+//   'showName',
+//   'showLabel',
+//   'showUMDs',
+//   'show433',
+//   'showTwins_KT',
+//   'showCampoIbarra',
+//   'showCampoAraya',
+//   'showUC',
+//   'showMARTA',
+//   'show433_1',
+//   'show433_2',
+//   'mult');
--- tank.js
+++ tank.js
@@ -1,57 +1,80 @@
-class Tank{
-    constructor(datos){
-        this.name = datos.name;
-        this.lsid = datos.lsid
-        this.pos = datos.pos
-        this.point = {};
-        this.id1 = datos.id1
-        this.ra1 = datos.ra1
-        this.rd1 = datos.rd1
-        this.pa1 = datos.pa1
-        this.id2 = datos.id2
-        this.ra2 = datos.ra2
-        this.rd2 = datos.rd2
-        this.pa2 = datos.pa2
-        this.id3 = datos.id3
-        this.ra3 = datos.ra3
-        this.rd3 = datos.rd3
-        this.pa3 = datos.pa3
-        this.radio_mikrotik = datos.radio_mikrotik
-        this.ip = datos.ip
-        this.cap_hs = datos.cap_hs
-        this.radio_uptime = datos.radio_uptime
-        this.front_end = datos.front_end
-        this.ekit1 = datos.ekit1
-        this.ekit2 = datos.ekit2
-        this.ekit3 = datos.ekit3
-        this.amiga_box = datos.amiga_box
-        this.tx = datos.tx
-        this.dist = datos.dist
-        this.bbox = datos.bbox
-        this.tipo = datos.tipo
-        this.terminado = datos.terminado
+class Tank {
+  constructor(datos) {
+    this.name = datos.name;
+    this.lsid = datos.lsid
+    this.pos = datos.pos
+    this.point = {};
+    this.id1 = datos.id1
+    this.ra1 = datos.ra1
+    this.rd1 = datos.rd1
+    this.pa1 = datos.pa1
+    this.id2 = datos.id2
+    this.ra2 = datos.ra2
+    this.rd2 = datos.rd2
+    this.pa2 = datos.pa2
+    this.id3 = datos.id3
+    this.ra3 = datos.ra3
+    this.rd3 = datos.rd3
+    this.pa3 = datos.pa3
+    this.radio_mikrotik = datos.radio_mikrotik
+    this.ip = datos.ip
+    this.cap_disipador = datos.cap_disipador
+    this.radio_uptime = datos.radio_uptime
+    this.front_end = datos.front_end
+    this.ekit1 = datos.ekit1
+    this.ekit2 = datos.ekit2
+    this.ekit3 = datos.ekit3
+    this.amiga_box = datos.amiga_box
+    this.tx = datos.tx
+    this.dist = datos.dist
+    this.bbox = datos.bbox
+    this.tipo = datos.tipo
+    this.observaciones = datos.observaciones
+    this.terminado = datos.terminado
 
-        this.caseLabel = "_-_";
-        this.textID;
-        this.textsize;
+    this.caseLabel = "_-_";
+    this.textID;
+    this.textsize;
 
-        this.radius = 1.8;
-      }
-      
-      update(){
-        this.point = AMIGA_Map.latLngToPixel(this.pos.lat,this.pos.lng)
-      }
+    this.radius = 1.8;
 
-      showSD(scl, item, label, name, lsid){
-        // Get fill color acording to item
-        this.getColor(item);
-        // Draw SD
-        circle(this.point.x, this.point.y, this.radius * 2 * scl * mult);
-        // Draw label text
-        if (label) {
-          text(this.caseLabel, this.point.x, this.point.y + this.radius * 2 * scl * mult);
-        }
-        // Draw Name and/or LSID
+    this.plot = true;
+    this.selected = false;
+  }
+
+  update() {
+    this.point = AMIGA_Map.latLngToPixel(this.pos.lat, this.pos.lng)
+  }
+
+  showSD(scl, item, label, name, lsid) {
+    this.textsize = constrain(3 * scl * propiedades.mult, 1, 18);
+
+    if (this.selected){
+      push();
+      textAlign(CENTER, CENTER);
+      fill(colors.selected);
+      textSize(3 * scl * propiedades.mult);
+      noStroke();
+      text("<      >", this.point.x, this.point.y);
+      pop();
+      strokeWeight(0.8 * scl * propiedades.mult);
+      stroke(colors.selected);
+    }
+
+    // Get fill color acording to item
+    this.getColor(item);
+    // Draw SD
+    circle(this.point.x, this.point.y, this.radius * 2 * scl * propiedades.mult);
+    imageMode(CENTER);
+    // tint(255,0,0,127);
+    // image(sd_img,this.point.x, this.point.y, this.radius * 2 * scl * propiedades.mult, this.radius * 2 * 1.185 * scl * propiedades.mult);
+    // Draw label text
+    if (label) {
+      textAlign(CENTER, TOP);
+      noStroke();
+      text(this.caseLabel, this.point.x, this.point.y + this.radius * 1.5 * scl * propiedades.mult);
+    }
+    // Draw Name and/or LSID
 
     if (name == true && lsid == true) {
       this.textID = this.name + " (" + this.lsid + ")";
@@ -63,95 +86,141 @@ class Tank{
       this.textID = "(-_-)";
     }
 
-    this.textsize = 3 * scl * mult;
+    textAlign(CENTER, BOTTOM);
     textSize(this.textsize);
-    fill(51);
+    fill(127);
     noStroke();
-    text(this.textID, this.point.x, this.point.y - this.radius * 2 * scl * mult);
-      }
+    text(this.textID, this.point.x, this.point.y - this.radius * 1.5 * scl * propiedades.mult);
 
-      showUMD(scl){
-        this.update();
-        //M101
-        let M101 = new UMD(this.point.x, this.point.y, this.ra1, this.rd1, this.pa1);
-        M101.show(scl * mult);
-        let M102 = new UMD(this.point.x, this.point.y, this.ra2, this.rd2, this.pa2);
-        M102.show(scl * mult);
-        let M103 = new UMD(this.point.x, this.point.y, this.ra3, this.rd3, this.pa3);
-        M103.show(scl * mult);
-        // M101.drawLine();
-        //M101
-        // let M102 = new UMD(90, 87, 164);
-        // M102.show();
-        //M101
-        // let M103 = new UMD(90, 102, 166);
-        // M103.show();
-      }
+    
+     
+  }
+// Draw popup
+  showPopup(){
+    if (dist(mouseX, mouseY, this.point.x, this.point.y) < 10) {
+      push();
+      textAlign(LEFT);
+      stroke(255,255,191);
+      strokeWeight(2);
+      fill(0, 240);
+    	rect(mouseX+10, mouseY+10, 170,80,7);
+      fill(255);
+      textSize(14);
+      noStroke();
+      text("Name: " + this.name, mouseX + 20, mouseY + 30);
+      text("LSID: " + this.lsid, mouseX + 20, mouseY + 50);
+      text("IP: " + this.ip, mouseX + 20, mouseY + 70);
+      // text(this.observaciones, mouseX + 20, mouseY + 70);
+      pop();
+    }
+  }
 
-      getColor(option){
-        switch (option) {
-          case 'amiga_box':
-      this.caseLabel = this.amiga_box;
-      if (this.caseLabel !== "" && this.caseLabel !== "-") {
-        fill(colors.ok);
-      } else if(this.caseLabel === "" ){
-        fill(colors.warning);
-      }
-      else if(this.caseLabel === "-" ){
-        fill(100, 20);
-      }
-      else{
-        fill(colors.noData);
-      }
-      break;
+  selectSD(){
+    if (dist(mouseX, mouseY, this.point.x, this.point.y) < 10) {
+      this.selected = !this.selected;      
+    }
+  }
 
-    case 'cap_hs':
-      this.caseLabel = this.cap_hs;
-      if (this.caseLabel === "OK") {
-        fill(colors.ok);
-      } else if (this.caseLabel === "CAP") {
-        fill("blue");
-      } else if (this.caseLabel === "" && this.radio_mikrotik === "OK") {
-        fill(colors.warning);
-      } else {
-        fill(colors.noData);
-      }
-      break;
+  showUMD(scl) {
+    this.update();
+    //M101
+    let M101 = new UMD(this.point.x, this.point.y, this.ra1, this.rd1, this.pa1, this.id1, this.ekit1, "M101");
+    M101.show(scl * propiedades.mult);
+    let M102 = new UMD(this.point.x, this.point.y, this.ra2, this.rd2, this.pa2, this.id2, this.ekit2, "M102");
+    M102.show(scl * propiedades.mult);
+    let M103 = new UMD(this.point.x, this.point.y, this.ra3, this.rd3, this.pa3, this.id3, this.ekit3, "M103");
+    M103.show(scl * propiedades.mult);
+    // M101.drawLine();
+    //M101
+    // let M102 = new UMD(90, 87, 164);
+    // M102.show();
+    //M101
+    // let M103 = new UMD(90, 102, 166);
+    // M103.show();
+  }
 
-    case 'terminado':
-      this.caseLabel = this.terminado;
-      if (this.caseLabel === "Terminado") {
-        fill(colors.ok);
-      } else {
-        fill(colors.noData);
-      }
-      break;
-
-    case 'radio_uptime':
-      this.caseLabel = this.radio_uptime;
-      if (this.caseLabel === "OK") {
-        fill(colors.ok);
-      } else if (this.caseLabel === "DEAD") {
-        fill(colors.dead);
-      } else if (this.caseLabel === "REBOOT") {
-        fill(colors.warning);
-      } else {
-        fill(colors.noData);
-      }
-      break;
-
-    case 'ip':
-      this.caseLabel = this.ip;
-      if (this.caseLabel !== "" && this.caseLabel !== "-") {
-        fill(colors.ok);
-      } 
-      else if(this.caseLabel === "-" ){
-        fill(100, 20);
-      }
-      else {
-        fill(colors.noData);
-      }
-      break;
+  getColor(option) {
+    switch (option) {
+      case 'amiga_box':
+        this.caseLabel = this.amiga_box;
+        if (this.caseLabel !== "" && this.caseLabel !== "-") {
+          fill(colors.ok);
+        } else if (this.caseLabel === "") {
+          fill(colors.warning);
+        } else if (this.caseLabel === "-") {
+          fill(100, 20);
+        } else {
+          fill(colors.noData);
         }
-      }
+        break;
+
+      case 'cap_disipador':
+        this.caseLabel = this.cap_disipador;
+        if (this.caseLabel === "OK") {
+          fill(colors.ok);
+        } else if (this.caseLabel === "CAP") {
+          fill("blue");
+        } else if (this.caseLabel === "OLD") {
+          fill(colors.warning);
+        } else {
+          fill(colors.noData);
+        }
+        break;
+
+      case 'terminado':
+        this.caseLabel = this.terminado;
+        if (this.caseLabel === "Terminado") {
+          fill(colors.ok);
+        } else if(this.caseLabel === "Casi"){
+          fill(colors.warning);
+        } else {
+          fill(colors.noData);
+        }
+        break;
+
+      case 'front_end':
+        this.caseLabel = this.front_end;
+        if (this.caseLabel === "Cyclone") {
+          fill(colors.ok);
+        } else if (this.caseLabel === "3.3" || this.caseLabel === "3.1" ) {
+          fill(colors.warning);
+        } else {
+          fill(colors.noData);
+        }
+        break;
+        
+        case 'radio_uptime':
+          this.caseLabel = this.radio_uptime;
+          if (this.caseLabel === "OK") {
+            fill(colors.ok);
+          } else if (this.caseLabel === "DEAD") {
+            fill(colors.dead);
+          } else if (this.caseLabel === "REBOOT") {
+            fill(colors.warning);
+          } else {
+            fill(colors.noData);
+          }
+          break;
+
+      case 'ip':
+        this.caseLabel = this.ip;
+        if (this.caseLabel !== "" && this.caseLabel !== "-") {
+          fill(colors.ok);
+        } else if (this.caseLabel === "-") {
+          fill(100, 20);
+        } else {
+          fill(colors.noData);
+        }
+        break;
+
+      case 'observaciones':
+        this.caseLabel = this.observaciones;
+        if (this.caseLabel == "" || this.caseLabel == "-") {
+          fill(colors.noData);
+        } else {
+          fill(colors.warning);
+        }
+        break;
+    }
+  }
 }
